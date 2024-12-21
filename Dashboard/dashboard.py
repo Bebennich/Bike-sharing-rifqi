9cd96d42433610b168918e003f303f2132b7c9ed
Bebennich <60698873+Bebennich@users.noreply.github.com>
2024-12-21 21:17:33 +0700

    Add files via upload

---
+++ Dashboard/dashboard.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+sns.set_theme(style='dark')
+
+def total_rent_by_days(df):
+    rent_by_hour = df.groupby(by=["weekday"], observed=False).agg({"count": ["sum"]})
+    return rent_by_hour
+
+def analysis_rent_by_days(df):
+    avg_rent = df.groupby("weekday", observed=True)["count"].mean().sort_values(ascending=False).reset_index()
+    return avg_rent
+
+def rent_by_weather(df):
+    rent_by_weather = df.groupby("weather_category", observed=True)["count"].nunique().sort_values(ascending=False).reset_index()
+    return rent_by_weather
+
+dataset_bike = pd.read_csv("Dashboard/day_dataset.csv")
+datetime_columns = ['dateday']
+dataset_bike.sort_values(by="dateday", inplace=True)
+dataset_bike.reset_index(inplace=True)
+
+for column in datetime_columns:
+    dataset_bike[column] = pd.to_datetime(dataset_bike['date'])
+
+min_date = dataset_bike['dateday'].min()
+max_date = dataset_bike['dateday'].max()
+
+with st.sidebar:
+    #logo company
+    st.image("https://images.unsplash.com/photo-1496147433903-1e62fdb6f4be?q=80&w=1421&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+    #Mengambil start date dan end date
+    start_date, end_date = st.date_input(
+        label='Rentang tanggal',
+        min_value=min_date,
+        max_value=max_date,
+        value=[min_date,max_date]
+    )
+    df_by_days = dataset_bike[(dataset_bike['dateday'] >= str(start_date)) & 
+                              (dataset_bike['dateday'] <= str(end_date))]
+    df_rent_by_days = total_rent_by_days(df_by_days)
+    df_analysis_rent_days = analysis_rent_by_days(df_by_days)
+    df_rent_by_weather = rent_by_weather(df_by_days)
+    
+st.header('Bike Sharing')
+st.subheader('Jumlah penyewaan sepeda Mingguan')
+total_rent = df_rent_by_days.count.sum()
+st.metric("Jumah penyewaan ", value=total_rent)
+        
+st.subheader("hari dengan rata-rata penyewaan sepeda terbanyak")
+colors = ['#A5DD9B', '#C5EBAA', '#F6F193', '#F2C18D']
+plt.figure(figsize=(10, 5))
+
+sns.barplot(
+    y="avg_peminjaman", 
+    x="hari",
+    data=df_analysis_rent_days.sort_values(by="avg_peminjaman", ascending=False).head(3),
+    palette=colors
+)
+plt.ylabel(None)
+plt.xlabel(None)
+plt.tick_params(axis='x', labelsize=12)
+st.pyplot(plt)
+
+st.subheader("Jumlah pelanggan berdasarkan kondisi cuaca")
+df_rent_by_weather
+colors = ['#FFBE98', '#FEECE2', '#F7DED0', '#E2BFB3']
+plt.figure(figsize=(10, 5))
+sns.barplot(
+    y="count", 
+    x="weather_catehory",
+    data=df_rent_by_weather.sort_values(by="count", ascending=False),
+    palette=colors
+)
+plt.ylabel(None)
+plt.xlabel(None)
+plt.tick_params(axis='x', labelsize=12)
+plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
+st.pyplot(plt)
---
+++ README.md
@@ -0,0 +1,52 @@
+Nama: Muhammad Rifqi Rahmanda
+Email: rifqirahmanda2004@gmail.com / 22081010315@student.upnjatim.ac.id
+ID Dicoding: Muhammad Rifqi Rahmanda
+
+# Bike Sahirng Analisis
+
+## Live Dashboard
+[Bike Sharing Dashboard](https://bikesharing-3tlvn2citvhvljt587rjvb.streamlit.app/)
+
+## Project Overview
+This project, submitted for the "Learn Data Analysis with Python" course from Dicoding, focuses on analyzing bike sahring data.
+## Introduction
+The goal of this project is to analyze bike sharing for What are annual bicycle usage trends, measured in days and hours? and What weather factor has the greatest influence on the number of bicycle rental companies?
+
+## Data Source
+The dataset used in this project includes bike sharing other related environmental data.
+
+## Libraries Used
+- Streamlit
+- Pandas
+- Matplotlib
+- Seaborn
+- NumPy
+
+## Key Insights
+- Seasonal variation in PM2.5 levels with higher concentrations in colder months.
+- Correlation between PM2.5 levels and weather conditions like temperature and humidity.
+- Trends and patterns revealed through time series analysis.
+
+## How to Run the Bike Sharing Analysis Dashboard
+To run the Bike Sharing Analysis Dashboard, follow these steps:
+
+## Create and Activate a Python Environment:
+If using Conda (ensure Conda is installed):
+ conda create --name bikesharing-ds python=3.9
+ conda activate bike_sharing-ds
+
+If using venv (standard Python environment tool):
+
+ python -m venv bike_sharing-ds
+
+## Install Required Packages:
+The following packages are necessary for running the analysis and the dashboard:
+
+pip install pandas numpy matplotlib seaborn streamlit
+Run the Streamlit App
+Navigate to the project directory where dashboard/dashboard.py is located:
+
+cd path/to/your/project/directory
+streamlit run dashboard/dashboard.py
+
+The dataset used for this analysis is included in the project repository. A detailed Python notebook (e.g., Proyek Analisi Data.ipynb) containing the data analysis and visualizations is also provided.
